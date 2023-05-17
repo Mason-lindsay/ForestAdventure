38bee7c4e451fd9f545e4298b445c10ab6ba347a
200917179 <masonalexanderlindsay@gmail.com>
2023-05-17 11:58:26 -0700

    hi

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.btn_start);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/Path.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Path {
+
+    private Button[] buttons;
+    private ImageView image;
+    private String text;
+
+    public Path(Button[] buttons, ImageView image, String text){
+        this.buttons = buttons;
+        this.image = image;
+        this.text = text;
+    }
+
+
+
+}
